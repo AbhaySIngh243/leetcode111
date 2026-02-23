d56420b1b3917d48c606522ac695fe2663abc0b8
AbhaySIngh243 <149238635+AbhaySIngh243@users.noreply.github.com>
2026-02-23 14:46:39 +0530

    Time: 0 ms (100.00%) | Memory: 25.6 MB (62.10%) - LeetSync

---
+++ 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double minimumAverage(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        double min_avg = 1e18;
+        int j = nums.size() - 1;
+
+        for (int i = 0; i < nums.size() / 2; i++) {
+            double avg = (nums[i] + nums[j]) / 2.0;
+            min_avg = min(min_avg, avg);
+            j--;
+        }
+
+        return min_avg;
+    }
+};
