3d09038be26374b23173c43402f7e0f5998b13df
AbhaySIngh243 <149238635+AbhaySIngh243@users.noreply.github.com>
2026-06-29 15:54:57 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (82.83%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
@@ -1,19 +1,8 @@
 class Solution {
 public:
-    int kthGrammar(int n, int k) {     
-      bool areValuesSame = true; 
-        n = pow(2, n - 1);
- 
-        while (n != 1) {
-           
-            n /= 2;
-
-           
-            if (k > n) {
-                k -= n;
-                areValuesSame = !areValuesSame;
-            }
-        }      
-        return ((areValuesSame) ? 0 : 1) ;
+    int kthGrammar(int N, int K) {
+	if (N == 1) return 0;
+	if (K % 2 == 0) return (kthGrammar(N - 1, K / 2) == 0) ? 1 : 0;
+	else return (kthGrammar(N - 1, (K + 1) / 2) == 0) ? 0 : 1;
     }
 };
